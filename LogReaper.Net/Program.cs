386993cce6e3f3679db25ba2bff2e7db950ae093
Debugg3R <debugg3r@gmail.com>
2022-06-29 23:36:51 +0300

    init commit

---
+++ LogReaper.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogReaper.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
